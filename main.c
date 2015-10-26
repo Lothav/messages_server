124772c74b0820d72ccd0aca99e38dc815bcbea0
Luiz0tavio <luizotavioresende@hotmail.com>
2015-10-26 17:48:18 +0000

    chat project

---
+++ main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: root
+ *
+ * Created on October 26, 2015, 5:40 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("asdas");
+
+    return (EXIT_SUCCESS);
+}
+
