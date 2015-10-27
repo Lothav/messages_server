32448dd5bc994bfe206323a7dd53b799e555d458
root <root@localhost.localdomain>
2015-10-26 22:03:13 -0200

    message struct

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /* 
  * File:   main.c
- * Author: root
+ * Author: Luiz Otavio
  *
  * Created on October 26, 2015, 5:40 PM
  */
@@ -8,12 +8,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
+#define MIN 0 
+#define MAX 500
+
+typedef struct message_st{
+    char message[MAX];
+    int pair_id;
+    int order;
+    struct message_st *prox;
+} message;
+
 int main(int argc, char** argv) {
     
-    printf("asdas");
+    message *root, *end;
+    
+    root = (message *) malloc(sizeof(message));
+    end = (message *) malloc(sizeof(message));
+    
+    
+    
 
     return (EXIT_SUCCESS);
 }
