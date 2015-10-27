4114e7157f9f526dcff12cb71e320e1b1df6e724
Luiz0tavio <luizotavioresende@hotmail.com>
2015-10-27 00:52:29 -0200

    structure to list messages

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MIN 0 
 #define MAX 500
@@ -15,17 +16,60 @@ typedef struct message_st{
     char message[MAX];
     int pair_id;
     int order;
-    struct message_st *prox;
-} message;
+} Message;
+
+typedef struct list_st{    
+    struct list_st *prox;
+    Message message;
+}List;
+
+void insertOnList(List *root, Message *message){
+
+    List *new;
+    
+    new = (List *) malloc(sizeof(List));
+    new->message.order = message->order;
+    new->message.pair_id = message->pair_id;
+    new->prox = NULL;
+    
+    while(root->prox != NULL){    
+        root = root->prox;
+    }
+    
+    root->prox = new;
+}
 
 int main(int argc, char** argv) {
     
-    message *root, *end;
+    List *root;
+    Message *message;
+    int k, lot_number;
+    char lot[11], msg[MAX];
+    char aux[11],aux2[11];
     
-    root = (message *) malloc(sizeof(message));
-    end = (message *) malloc(sizeof(message));
+    scanf("%d", &k);
+    if( k < 1 || k > 1000 ){
+        printf("\n k is out of bounds");
+        return (EXIT_FAILURE);
+    }
     
-
+    root = (List *) malloc(sizeof(List));    
+  
+    scanf("%s", lot);
+    while(lot != "-1"){
+        scanf("%d", &lot_number);
+        scanf(" %[^;];%[^;];%[^\n]", aux, aux2, msg);
+        
+        while(msg != "Fim"){
+            
+           root->message.pair_id = atoi(aux);
+           root->message.order = atoi(aux2);
+           strcpy(root->message.message, msg);
+           scanf(" %[^;];%[^;];%[^\n]", aux, aux2, msg);
+        }  
+    }
+    
+    
     return (EXIT_SUCCESS);
 }
 
