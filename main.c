30d5a98c2bf4c1623352fe0d22f9684dd063dd1b
Luiz0tavio <luizotavioresende@hotmail.com>
2015-11-05 23:43:25 -0200

    missing counters and k

--- main.c
+++ main.c
@@ -50,7 +50,7 @@ Conv* selectConv(Container* root, int pair_id){
     int id;
     
     //vamos pegar um 'aux' com a primeira conversa após a cabeça
-    aux = root->conv_head->prox;
+    aux = root->conv_head;
     
     //verifica já existe a conversa entre os pares 'pair_id'.
     while(aux != NULL){
@@ -80,6 +80,10 @@ Conv* selectConv(Container* root, int pair_id){
     aux->pair_id = pair_id;
     root->conv_last->prox = aux;
     root->conv_last = aux;
+    aux->prox = NULL;
+    aux->msg_head = NULL;
+    aux->msg_last = aux->msg_head;
+    aux->last_msg = 0;
     //e então o retornamos
     return aux;
 }
@@ -145,39 +149,136 @@ void insOnConv(Conv* conv, int ord, char* msg){
     //"inserindo" no final da lista:
     conv->msg_last->prox = aux;
     conv->msg_last = aux;
+    aux->prox = NULL;
 }
 
-void sendMessages(Container *root){
+
+int getMinMens(Container *root){
+
+    Conv * aux;
+    int min_mens = MAX;
+    
+    aux = root->conv_head->prox;
+    
+    while(aux != NULL){
+	
+	if(aux->msg_head->prox != NULL && aux->last_msg < min_mens && aux->msg_head->prox->order == aux->last_msg + 1){
+	    min_mens = aux->last_msg;
+	}
+	aux = aux->prox;
+    }
+    
+    return min_mens;
+}
+
+
+void sendMessages(Container *root, int total_msgs) {
 
     // As prioridade ao enviar as mensagens são:
     //   1 -> Quantidade de mensagens enviadas pelo par.
     // caso empate:  
     //   2 -> ID do par
-    
-    Conv* aux;
-    
-    aux = root->conv_head->prox;
-    
-    while(aux != NULL){
-        
-        
-    
-    }
-    
-    
-    
-    
-    
-    
-    
 
+    Conv* aux, *find_min_answ;
+    Message* to_send;
+
+    int a;
+
+    // min_answ representa a quantidade de envios
+    // do par que trocou menos mensagens.
+    int min_answ = 0;
+
+    //verificação se o min_answ precisa ser calculado novamente
+
+    printf("Envios:\n");
+    
+    int re_min_answ = 1;
+
+    for (a = 0; a < total_msgs; a++) {
+	
+	// percorrendo as conversas:
+	aux = root->conv_head->prox;
+	while (aux != NULL) {
+
+	    if (re_min_answ == 1) {
+		min_answ = getMinMens(root);
+		// após encontramos o min_answ, setemos 0 na verificação,
+		// para que só seja feita novamente quando uma mensagem
+		// for enviada.
+		re_min_answ = 0;
+	    }
+	    to_send = aux->msg_head->prox;
+	    // se a ordem da mensagem for a menor quantidade + 1
+	    // e se a quantidade de mensagens enviadas pela conversa for a menor
+	    // ela é enviada.
+	    if (to_send != NULL && to_send->order == aux->last_msg + 1 && min_answ == aux->last_msg) {
+		// imprime os dados da mensagem na tela
+		printf("%d;%d;%s\n", to_send->pair_id, to_send->order, to_send->message);
+		// muda a cabeça da lista para a mensagem após a que foi enviada
+		aux->msg_head->prox = to_send->prox;
+		if(to_send->prox == NULL){
+		    aux->msg_last = aux->msg_head;
+		}
+		// já que a mensagem fora enviada, podemos liberar o espaço alocado
+		free(to_send);
+		// é adicionado +1 na contagem de mensagens enviadas pela Conv
+		aux->last_msg++;
+
+		// Parte importante:
+		// após enviar uma mensagem, todo o while deve recomeçar
+		// pois pode ter uma conversa pra trás que será a próx
+		// a enviar, logo :
+		aux = root->conv_head->prox;
+		min_answ = MAX;
+		re_min_answ = 1;
+	    }
+	    if(re_min_answ == 0){
+		aux = aux->prox;
+	    }
+	}
+    }
+}
+
+
+int printListsAndCount(Container* root){
+
+    Conv* aux_conv;
+    Message* aux_msg;
+    
+    int total_mens = 0;
+    
+    aux_conv = root->conv_head->prox;
+    
+    printf("Listas:\n");
+    
+    while(aux_conv != NULL){
+    
+	printf("Par_%d:[", aux_conv->pair_id);
+	
+	aux_msg = aux_conv->msg_head->prox;
+
+	while(aux_msg != NULL){
+	    
+	    if(aux_msg->prox != NULL){
+		printf("(%d,%s),", aux_msg->order, aux_msg->message);
+		total_mens++;
+	    }else{
+		printf("(%d,%s)", aux_msg->order, aux_msg->message);
+		total_mens++;
+	    }
+	    aux_msg = aux_msg->prox;
+	}
+	printf("]\n");
+	aux_conv = aux_conv->prox;
+    }
+    return total_mens;
 }
 
 
 
 int main(int argc, char** argv) {
 
-    int lot_number, k, pair_id, ord;
+    int lot_number, k, pair_id, ord, total_msgs;
     char str[MAX], msg[MAX];
     Container *root;
     Conv *conv;
@@ -225,7 +326,8 @@ int main(int argc, char** argv) {
         
         
         // >>>>>>>> Enviando as Mensagens do Lote <<<<<<
-       // sendMessages(root);
+	total_msgs = printListsAndCount(root);
+        sendMessages(root, total_msgs);
 
         
         
@@ -249,38 +351,6 @@ int main(int argc, char** argv) {
     }
     return (EXIT_SUCCESS);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -314,227 +384,3 @@ Fim
  
  
  */
-
-
-
-/*void insertMessages(Pair *pair, int id, int order, char* msg) {
-
-    Pair *aux;
-
-    if (pair->msg_head == NULL || pair->msg_head->prox == NULL) {
-	pair->msg_head = (Message *) malloc(sizeof (Message));
-	pair->msg_last = pair->msg_head;
-	pair->msg_head->pair_id = MAX;
-	pair->msg_head->order = MAX;
-    }
-
-    pair->msg_last->prox = (Message *) malloc(sizeof (Message));
-    pair->msg_last->prox->ant = pair->msg_last;
-    pair->msg_last->prox->ant->prox = pair->msg_last->prox;
-
-    pair->msg_last = pair->msg_last->prox;
-    pair->pair_id = id;
-    strcpy(pair->msg_last->message, msg);
-    pair->msg_last->pair_id = id;
-    pair->msg_last->order = order;
-
-}
-
-void printLists(PairsArr *root) {
-
-    Pair *pair_root;
-    Message *aux_msg;
-
-    int a, has, b, high;
-
-    pair_root = root->pair_head;
-
-    printf("Listas:\n");
-
-    for (a = 0; a < 50; a++) {
-	has = 0;
-	pair_root = root->pair_head;
-
-	while (pair_root != NULL) {
-	    if (pair_root->pair_id == a) {
-		has = 1;
-		break;
-	    }
-	    pair_root = pair_root->prox;
-	}
-
-	if (has == 1) {
-	    high = MIN;
-	    
-	    aux_msg = pair_root->msg_head->prox;
-		while (aux_msg != NULL) {
-		    if(aux_msg->order > high){
-			high = aux_msg->order;
-		    }
-		    aux_msg = aux_msg->prox;
-		}
-	    
-	    printf("Par_%d:[", a);
-	    for (b = 0; b < 50; b++) {
-		
-		aux_msg = pair_root->msg_head;
-
-		while (aux_msg != NULL) {
-		    if (aux_msg->order == b) {
-			if (aux_msg->order == high) {
-			    printf("(%d,%s)", b, aux_msg->message);
-			} else {
-			    printf("(%d,%s), ", b, aux_msg->message);
-			}
-		    }
-		    aux_msg = aux_msg->prox;
-		}
-	    }
-	    printf("]\n");
-	}
-    }
-}
-
-void printSends(PairsArr *root) {
-
-    Pair *pair_root;
-    Message *aux_msg, *swit, *loop;
-
-    int a, has, b, c, lowest_msg, low_order;
-
-    pair_root = root->pair_head;
-
-
-    printf("Envios:\n");
-
-    for (c = 0; c < 50; c++) {
-
-	for (a = 0; a < 50; a++) {
-	    has = 0;
-
-	    pair_root = root->pair_head->prox;
-	    lowest_msg = MAX;
-	    while (pair_root->prox != NULL) {
-		if (pair_root->last_msg < lowest_msg) {
-		    lowest_msg = pair_root->last_msg;
-		}
-		pair_root = pair_root->prox;
-	    }
-
-	    pair_root = root->pair_head->prox;
-
-	    while (pair_root != NULL) {
-		if (pair_root->pair_id == a) {
-		    has = 1;
-		    break;
-		}
-		pair_root = pair_root->prox;
-	    }
-	    
-	    if (has == 1) {
-		for (b = 0; b < 50; b++) {
-		    aux_msg = pair_root->msg_head->prox;
-		    while (aux_msg != NULL) {
-			if (aux_msg->order == b) {
-			    if (pair_root->last_msg <= lowest_msg && pair_root->last_msg == aux_msg->order-1) {
-				printf("%d;%d;%s\n", a, b, aux_msg->message);
-				aux_msg->ant->prox = aux_msg->prox;
-				if (aux_msg->prox != NULL) {
-				    aux_msg->prox->ant = aux_msg->ant;
-				}
-				pair_root->last_msg++;
-			    }
-			}
-			aux_msg = aux_msg->prox;
-		    }
-		}
-	    }
-	}
-    }
-}
-
-void printCounts(PairsArr *root) {
-
-    Pair *paux;
-    int a;
-
-    printf("Contadores:\n");
-    for (a = 0; a < 50; a++) {
-	paux = root->pair_head->prox;
-	while (paux != NULL) {
-	    if (paux->pair_id == a) {
-		printf("Par_%d:%d\n", a, paux->last_msg);
-	    }
-	    paux = paux->prox;
-	}
-    }
-}
-
-int main(int argc, char** argv) {
-
-    int lot_number;
-    char lot[11], str[MAX], msg[MAX];
-    int id, ord, is_new, head_c = 0, k;
-    PairsArr *root, *head, *queue;
-    Pair *auxP;
-
-    root = (PairsArr *) malloc(sizeof (PairsArr));
-
-    root->pair_head = (Pair *) malloc(sizeof (Pair));
-    root->pair_last = root->pair_head;
-    root->pair_head->pair_id = MAX;
-
-    fgets(str, sizeof (str), stdin);
-    sscanf(str, "%d", &k);
-    fflush(stdin);
-
-    fgets(str, sizeof (str), stdin);
-    sscanf(str, "%*[^0-9]%d", &lot_number);
-    fflush(stdin);
-
-    fgets(str, sizeof (str), stdin);
-    sscanf(str, "%d;%d;%[^\n]s", &id, &ord, msg);
-    fflush(stdin);
-
-    while (lot_number != -1) {
-	while (strcmp(str, "Fim\n") != 0) {
-
-	    auxP = root->pair_head;
-	    is_new = 1;
-	    while (auxP->prox != NULL) {
-		if (auxP->pair_id == id) {
-		    is_new = 0;
-		    break;
-		}
-		auxP = auxP->prox;
-	    }
-
-	    if (auxP->pair_id == id && auxP->prox == NULL) {
-		is_new = 0;
-	    }
-
-	    if (is_new == 1) {
-		auxP->prox = (Pair *) malloc(sizeof (Pair));
-		auxP = auxP->prox;
-	    }
-
-	    insertMessages(auxP, id, ord, msg);
-
-	    fgets(str, sizeof (str), stdin);
-	    sscanf(str, "%d;%d;%[^\n]s", &id, &ord, msg);
-	    fflush(stdin);
-	}
-	printLists(root);
-	printSends(root);
-	printCounts(root);
-
-	fgets(str, sizeof (str), stdin);
-	sscanf(str, "%*[^0-9]%d", &lot_number);
-	fflush(stdin);
-
-	fgets(str, sizeof (str), stdin);
-	sscanf(str, "%d;%d;%[^\n]s", &id, &ord, msg);
-	fflush(stdin);
-    }
-    return (EXIT_SUCCESS);
-}*/
